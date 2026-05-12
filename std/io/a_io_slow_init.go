605383252d7416497a5a825d0cf29f27ef41aa3f
Rui Carmo <rui.carmo@gmail.com>
2026-05-12 18:49:35 +0100

    docs: update generated IO copy docs

--- std/io/a_io_slow_init.go
+++ std/io/a_io_slow_init.go
@@ -23,9 +23,9 @@ func InternsOrThunks() {
 		MakeMeta(
 			NewListFrom(NewVectorFrom(MakeSymbol("dst"), MakeSymbol("src"))),
 			`Copies from src to dst until either EOF is reached on src or an error occurs.
-  Returns the number of bytes copied or throws an error.
+  Returns the number of bytes copied as an Int, or BigInt if the count exceeds the native int range.
   src must be IOReader, e.g. as returned by joker.os/open.
-  dst must be IOWriter, e.g. as returned by joker.os/create.`, "1.0").Plus(MakeKeyword("tag"), String{S: "Int"}))
+  dst must be IOWriter, e.g. as returned by joker.os/create.`, "1.0"))
 
 	ioNamespace.InternVar("pipe", pipe_,
 		MakeMeta(
