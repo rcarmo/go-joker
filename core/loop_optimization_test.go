382bb3a41660de29e204f64c7c62cf5e6863bbb8
Rui Carmo <rui.carmo@gmail.com>
2026-05-15 00:42:49 +0100

    Consolidate loop frame regression test

--- core/loop_optimization_test.go
+++ core/loop_optimization_test.go
@@ -556,3 +556,18 @@ func TestIRSubs(t *testing.T) {
 		}
 	}
 }
+
+func TestIRGuessLoopFramePrefersRecurBindings(t *testing.T) {
+	expr := compileTestExpr(t, `(let [limit 4]
+  (loop [i 0 acc 0]
+    (if (< i limit)
+      (let [x (loop [j 0 s 0]
+                (if (= j i) s (recur (inc j) (+ s j))))]
+        (recur (inc i) (+ acc x)))
+      acc)))`)
+	outer := expr.(*LetExpr).body[0].(*LoopExpr)
+	if prog := irCompile(outer); prog == nil {
+		t.Fatal("expected outer loop with nested loop/captures to compile")
+	}
+	requireInt(t, Eval(expr, nil), 4)
+}
