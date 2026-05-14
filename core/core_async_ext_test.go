2d98cb9a21a5d98917e62252998326ab49a02954
Rui Carmo <rui.carmo@gmail.com>
2026-05-15 00:32:20 +0100

    Consolidate channel contract test

--- core/core_async_ext_test.go
+++ core/core_async_ext_test.go
@@ -1,6 +1,28 @@
 package core
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
+
+func TestChannelCloseIsIdempotentUnderConcurrency(t *testing.T) {
+	ch := MakeChannel(make(chan FutureResult, 1))
+	var wg sync.WaitGroup
+	for i := 0; i < 16; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			ch.Close()
+		}()
+	}
+	wg.Wait()
+	if !ch.IsClosed() {
+		t.Fatal("channel should report closed after concurrent Close calls")
+	}
+	if ch.Send(MakeInt(1)) {
+		t.Fatal("Send on closed channel should return false")
+	}
+}
 
 func TestCoreAsyncNamespaceAliases(t *testing.T) {
 	requireInt(t, evalTestScript(t, `(let [c (clojure.core.async/chan 1)]
