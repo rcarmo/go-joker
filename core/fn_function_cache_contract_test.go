e2bd619fb2a65c10e0fbde164658c87455fdf0a9
Rui Carmo <rui.carmo@gmail.com>
2026-05-15 00:25:39 +0100

    Consolidate function IR fast path test

--- core/fn_function_cache_contract_test.go
+++ core/fn_function_cache_contract_test.go
@@ -33,3 +33,13 @@ func TestIRFunctionCacheUsesStableVariadicKey(t *testing.T) {
 		t.Fatalf("variadic irCompileFn returned different programs for same fn: %p != %p", first, second)
 	}
 }
+
+func TestIREqSupportsStringsAndChars(t *testing.T) {
+	requireInt(t, evalTestScript(t, `(let [f (fn [c]
+                                  (if (= c "A") 1
+                                  (if (= c "T") 2 3)))]
+  (loop [i 0 acc 0]
+    (if (= i 3)
+      acc
+      (recur (inc i) (+ acc (f (str (nth "ATA" i))))))))`), 4)
+}
