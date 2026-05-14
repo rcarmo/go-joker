07369b4263c2a00fade6522ace77218c2c9808a4
Rui Carmo <rui.carmo@gmail.com>
2026-05-15 00:49:53 +0100

    Consolidate typed vector tests

--- core/typed_helpers_test.go
+++ core/typed_helpers_test.go
@@ -69,3 +69,23 @@ func TestIRTypedNestedStringLoop(t *testing.T) {
                   (recur (inc j) (str s (nth dna (+ i j))))))]
         (recur (inc i) (+ total (count k)))))))`), 6)
 }
+
+func TestIRTypedIntVector(t *testing.T) {
+	t.Setenv("JOKER_IR_TYPED_VEC", "1")
+	requireInt(t, evalTestScript(t, `(loop [i 0 v []]
+  (if (= i 5)
+    (+ (nth v 0) (nth v 4))
+    (recur (inc i) (conj v i))))`), 4)
+}
+
+func BenchmarkIRTypedIntVectorLoop(b *testing.B) {
+	expr := compileBenchExpr(b, `(loop [i 0 v []]
+  (if (= i 1000)
+    (+ (nth v 0) (nth v 999))
+    (recur (inc i) (conj v i))))`)
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = Eval(expr, nil)
+	}
+}
