35993b97ef09ba32d5a0b4b9c42830ec346ba19d
Rui Carmo <rui.carmo@gmail.com>
2026-05-15 00:21:49 +0100

    Fold generated data processing into procs

--- core/procs.go
+++ core/procs.go
@@ -2046,6 +2046,48 @@ func ProcessReaderFromEval(reader *Reader, filename string) {
 	}
 }
 
+var haveSetCoreNamespaces bool
+
+func ProcessCoreData() {
+	// Let MaybeLazy() handle initialization.
+	if !haveSetCoreNamespaces {
+		setCoreNamespaces()
+		haveSetCoreNamespaces = true
+	}
+}
+
+func ProcessReplData() {
+	// Let MaybeLazy() handle initialization.
+}
+
+func ProcessLinterData(dialect Dialect) {
+	if dialect == EDN {
+		markJokerNamespacesAsUsed()
+		return
+	}
+	processGeneratedLinterPayload("linter_all.joke")
+	if dialect == JOKER {
+		markJokerNamespacesAsUsed()
+		processGeneratedLinterPayload("linter_joker.joke")
+		return
+	}
+	processGeneratedLinterPayload("linter_cljx.joke")
+	switch dialect {
+	case CLJ:
+		processGeneratedLinterPayload("linter_clj.joke")
+	case CLJS:
+		processGeneratedLinterPayload("linter_cljs.joke")
+	}
+}
+
+func processGeneratedLinterPayload(path string) {
+	data, ok := coregenerated.LinterDataByPath(path)
+	if !ok {
+		panic(RT.NewError("missing generated linter payload: " + path))
+	}
+	processData(data)
+}
+
 func processData(data []byte) {
 	ns := GLOBAL_ENV.CurrentNamespace()
 	GLOBAL_ENV.SetCurrentNamespace(GLOBAL_ENV.CoreNamespace)
