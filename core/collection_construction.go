3ac1ef0ca06fdced49f1bcf12918ba9b1778d990
Rui Carmo <rui.carmo@gmail.com>
2026-05-14 08:35:12 +0100

    Define collection construction adapter

---
+++ core/collection_construction.go
@@ -0,0 +1,45 @@
+package core
+
+// CollectionConstructionAdapter is the narrow root-owned construction surface
+// for collection values. Future extraction of vectors/maps/sets should route
+// through this surface instead of scattering concrete constructor calls across
+// evaluator code.
+type CollectionConstructionAdapter struct{}
+
+var collections CollectionConstructionAdapter
+
+func (CollectionConstructionAdapter) EmptyVector() *Vector {
+	return EmptyVector()
+}
+
+func (CollectionConstructionAdapter) VectorFrom(objs ...Object) *Vector {
+	return NewVectorFrom(objs...)
+}
+
+func (CollectionConstructionAdapter) VectorFromSeq(seq Seq) *Vector {
+	return NewVectorFromSeq(seq)
+}
+
+func (CollectionConstructionAdapter) EmptyArrayVector() *ArrayVector {
+	return EmptyArrayVector()
+}
+
+func (CollectionConstructionAdapter) ArrayVectorFrom(objs ...Object) *ArrayVector {
+	return NewArrayVectorFrom(objs...)
+}
+
+func (CollectionConstructionAdapter) EmptyArrayMap() *ArrayMap {
+	return EmptyArrayMap()
+}
+
+func (CollectionConstructionAdapter) HashMapFrom(keyvals ...Object) *HashMap {
+	return NewHashMap(keyvals...)
+}
+
+func (CollectionConstructionAdapter) EmptySet() *MapSet {
+	return EmptySet()
+}
+
+func (CollectionConstructionAdapter) SetFromSeq(seq Seq) *MapSet {
+	return NewSetFromSeq(seq)
+}
