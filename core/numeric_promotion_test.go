4849c929ab5fd54739c554a3ed75a65b3feded09
Rui Carmo <rui.carmo@gmail.com>
2026-05-15 07:38:53 +0100

    Consolidate file info numeric promotion test

--- core/numeric_promotion_test.go
+++ core/numeric_promotion_test.go
@@ -1,9 +1,12 @@
 package core
 
 import (
+	"io/fs"
+	"math"
 	"math/big"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestIntArithmeticPromotesToBigIntOnOverflow(t *testing.T) {
@@ -50,5 +53,33 @@ func TestBigIntIntConvertsWithinNativeRange(t *testing.T) {
 	got := MakeBigInt(big.NewInt(42)).Int()
 	if got.I != 42 {
 		t.Fatalf("BigInt.Int = %d, want 42", got.I)
+	}
+}
+
+type contractFileInfo struct {
+	size int64
+}
+
+func (fi contractFileInfo) Name() string       { return "contract" }
+func (fi contractFileInfo) Size() int64        { return fi.size }
+func (fi contractFileInfo) Mode() fs.FileMode  { return 0o644 }
+func (fi contractFileInfo) ModTime() time.Time { return time.Unix(0, 0) }
+func (fi contractFileInfo) IsDir() bool        { return false }
+func (fi contractFileInfo) Sys() any           { return nil }
+
+func TestFileInfoMapPromotesLargeSize(t *testing.T) {
+	m := FileInfoMap("contract", contractFileInfo{size: math.MaxInt64})
+	found, got := m.Get(MakeKeyword("size"))
+	if !found {
+		t.Fatal("FileInfoMap missing :size")
+	}
+	if math.MaxInt64 > int64(maxInt) {
+		if got.GetType() != TYPE.BigInt {
+			t.Fatalf("file size type = %s, want BigInt", got.GetType().ToString(false))
+		}
+		return
+	}
+	if got.GetType() != TYPE.Int {
+		t.Fatalf("file size type = %s, want Int", got.GetType().ToString(false))
 	}
 }
