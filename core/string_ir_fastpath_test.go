293ef081ad8af2ab4adafa3457a914125dea5b7d
Rui Carmo <rui.carmo@gmail.com>
2026-05-15 05:34:53 +0100

    Consolidate string opcode test

--- core/string_ir_fastpath_test.go
+++ core/string_ir_fastpath_test.go
@@ -34,3 +34,28 @@ func TestCharToStringFast(t *testing.T) {
 		t.Fatalf("expected unicode string object, got %T %s", got, got.ToString(false))
 	}
 }
+
+func TestIRNthStringASCIIOpcode(t *testing.T) {
+	expr := compileTestExpr(t, `(let [dna "ACGT"]
+  (loop [i 0 acc ""]
+    (if (= i 4)
+      acc
+      (recur (inc i) (str acc (nth dna i))))))`)
+	letExpr := expr.(*LetExpr)
+	loop := letExpr.body[0].(*LoopExpr)
+	prog := irCompile(loop)
+	if prog == nil {
+		t.Fatal("expected IR")
+	}
+	found := false
+	for pc := 0; pc < len(prog.code); pc++ {
+		if prog.code[pc] == irNthStringASCII {
+			found = true
+			break
+		}
+	}
+	if !found {
+		t.Fatal("expected irNthStringASCII opcode")
+	}
+	requireString(t, Eval(expr, nil), "ACGT")
+}
