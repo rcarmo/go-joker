e8d6245c802076b03b33c4ed7ccd7b5712901a34
Rui Carmo <rui.carmo@gmail.com>
2026-05-15 07:49:15 +0100

    Consolidate typed value tests

--- core/typed_helpers_test.go
+++ core/typed_helpers_test.go
@@ -112,3 +112,33 @@ func TestIRTypedVectorNthForStringMap(t *testing.T) {
       (let [k (nth ks (rem i 2))]
         (recur (inc i) (assoc m k (inc (get m k 0))))))))`), 2)
 }
+
+func TestIRTypedStringLoop(t *testing.T) {
+	expr := compileTestExpr(t, `(let [dna "ACGT"]
+  (loop [i 0 s ""]
+    (if (= i 4)
+      (count s)
+      (recur (inc i) (str s (nth dna i))))))`)
+	letExpr := expr.(*LetExpr)
+	prog := irCompile(letExpr.body[0].(*LoopExpr))
+	if prog == nil {
+		t.Fatal("expected IR")
+	}
+	got := irExecTyped(prog, []Object{Int{I: 0}, String{S: ""}})
+	requireInt(t, got, 4)
+}
+
+func TestIRTypedStringBuilderSlot(t *testing.T) {
+	expr := compileTestExpr(t, `(let [dna "ACGT"]
+  (loop [i 0 s ""]
+    (if (= i 4)
+      s
+      (recur (inc i) (str s (nth dna i))))))`)
+	letExpr := expr.(*LetExpr)
+	prog := irCompile(letExpr.body[0].(*LoopExpr))
+	if prog == nil {
+		t.Fatal("expected IR")
+	}
+	got := irExecTyped(prog, []Object{Int{I: 0}, String{S: ""}})
+	requireString(t, got, "ACGT")
+}
