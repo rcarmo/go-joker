37408713cb3454ffae678cb70c84d21c415cb278
Rui Carmo <rui.carmo@gmail.com>
2026-05-15 05:22:00 +0100

    Consolidate IR inline tests

--- core/inline_slot_test.go
+++ core/inline_slot_test.go
@@ -24,3 +24,21 @@ func TestIRInlineSlotCollision(t *testing.T) {
 		// f(1,0)+f(2,1)+f(3,2)+f(4,3)+f(5,4) = 1+5+11+19+29 = 65
 		65)
 }
+
+func TestIRInlineSmallHelper(t *testing.T) {
+	t.Setenv("JOKER_IR_INLINE", "1")
+	requireInt(t, evalTestScript(t, `(let [f (fn [x] (+ x 1))]
+  (loop [i 0 acc 0]
+    (if (= i 4)
+      acc
+      (recur (inc i) (+ acc (f i))))))`), 10)
+}
+
+func TestIRInlineCollectionHelper(t *testing.T) {
+	requireInt(t, evalTestScript(t, `(let [pick (fn [v i] (+ (nth v i) 1))
+                                      xs [1 2 3 4]]
+  (loop [i 0 acc 0]
+    (if (= i 4)
+      acc
+      (recur (inc i) (+ acc (pick xs i))))))`), 14)
+}
