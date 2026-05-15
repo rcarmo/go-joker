d18fc5a4a9b5d1601408090c9f70698b714e5107
Rui Carmo <rui.carmo@gmail.com>
2026-05-15 05:38:00 +0100

    Consolidate typed map tests

--- core/typed_helpers_test.go
+++ core/typed_helpers_test.go
@@ -90,3 +90,25 @@ func TestIRTypedGenericStringNth(t *testing.T) {
 	got := irExecTyped(prog, []Object{Int{I: 0}, String{S: "ACGT"}, String{S: ""}})
 	requireString(t, got, "ACGT")
 }
+
+func TestIRTypedStringIntMap(t *testing.T) {
+	t.Setenv("JOKER_IR_TYPED_MAP", "1")
+	requireInt(t, evalTestScript(t, `(loop [i 0 m {}]
+  (if (= i 4)
+    (get m "aa" 0)
+    (recur (inc i) (assoc m "aa" (inc (get m "aa" 0))))))`), 4)
+	requireBool(t, evalTestScript(t, `(nil? (loop [i 0 m {}]
+  (if (= i 1)
+    (get m "missing")
+    (recur (inc i) m))))`), true)
+}
+
+func TestIRTypedVectorNthForStringMap(t *testing.T) {
+	t.Setenv("JOKER_IR_TYPED_MAP", "1")
+	requireInt(t, evalTestScript(t, `(let [ks ["aa" "bb"]]
+  (loop [i 0 m {}]
+    (if (= i 4)
+      (get m "aa" 0)
+      (let [k (nth ks (rem i 2))]
+        (recur (inc i) (assoc m k (inc (get m k 0))))))))`), 2)
+}
