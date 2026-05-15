8653a6ceac38a7a2d628feac26eeb2ee0bc758c9
Rui Carmo <rui.carmo@gmail.com>
2026-05-15 05:25:13 +0100

    Consolidate typed nth test

--- core/typed_helpers_test.go
+++ core/typed_helpers_test.go
@@ -77,3 +77,16 @@ func TestIRTypedIntVector(t *testing.T) {
     (+ (nth v 0) (nth v 4))
     (recur (inc i) (conj v i))))`), 4)
 }
+
+func TestIRTypedGenericStringNth(t *testing.T) {
+	expr := compileTestExpr(t, `(loop [i 0 s "ACGT" acc ""]
+  (if (= i 4)
+    acc
+    (recur (inc i) s (str acc (nth s i)))))`)
+	prog := irCompile(expr.(*LoopExpr))
+	if prog == nil {
+		t.Fatal("expected IR")
+	}
+	got := irExecTyped(prog, []Object{Int{I: 0}, String{S: "ACGT"}, String{S: ""}})
+	requireString(t, got, "ACGT")
+}
