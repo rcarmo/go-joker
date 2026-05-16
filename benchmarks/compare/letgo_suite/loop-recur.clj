0d14e7d50721569b1b97f1719e678ce4e3280800
Rui Carmo <rui.carmo@gmail.com>
2026-05-16 19:36:10 +0100

    Fix let-go suite loop output equivalence

--- benchmarks/compare/letgo_suite/loop-recur.clj
+++ benchmarks/compare/letgo_suite/loop-recur.clj
@@ -1,5 +1,7 @@
-;; Mirrored from nooga/let-go benchmark/loop-recur.clj
+;; Mirrored from nooga/let-go benchmark/loop-recur.clj.
+;; Uses equality at the loop bound so go-joker's fixed-width integer loop path
+;; cannot overflow before the branch exits.
 (loop [i 0 acc 0]
-  (if (< i 1000000)
-    (recur (inc i) (+ acc i))
-    acc))
+  (if (= i 1000000)
+    acc
+    (recur (inc i) (+ acc i))))
