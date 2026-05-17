bd971536cf1760910b6c8eff09e5fef3641d5a5a
Rui Carmo <rui.carmo@gmail.com>
2026-05-17 01:42:28 +0100

    Consolidate string adapter helpers

--- core/object.go
+++ core/object.go
@@ -1168,6 +1168,41 @@ func (bf *BigFloat) Compare(other Object) int {
 	return CompareNumbers(bf, EnsureObjectIsNumber(other, "Cannot compare BigFloat: %s"))
 }
 
+var asciiCharStringObjects = corestr.NewObjectCache(func(ch rune) Object {
+	return String{S: corestr.String(ch)}
+})
+
+func charToStringFast(ch rune) string { return corestr.String(ch) }
+
+func charToStringObjectFast(ch rune) Object {
+	if obj, ok := asciiCharStringObjects.Lookup(ch); ok {
+		return obj
+	}
+	return String{S: corestr.String(ch)}
+}
+
+func EnsureObjectIsStringable(obj Object, pattern string) String {
+	switch c := obj.(type) {
+	case String:
+		return c
+	case Char:
+		return String{S: string(c.Ch)}
+	default:
+		panic(FailObject(c, "Stringable", pattern))
+	}
+}
+
+func EnsureArgIsStringable(args []Object, index int) String {
+	switch c := args[index].(type) {
+	case String:
+		return c
+	case Char:
+		return String{S: string(c.Ch)}
+	default:
+		panic(FailArg(c, "Stringable", index))
+	}
+}
+
 func (c Char) ToString(escape bool) string {
 	if escape {
 		return corestr.EscapeRune(c.Ch)
